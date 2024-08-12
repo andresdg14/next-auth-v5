d62e2c10c3c94983ad64f40a69b737660c3807e5
Andres Dominguez <andresdomlez@gmail.com>
2024-08-13 00:02:40 +0100

    login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,6 +1,28 @@
+import * as z from 'zod';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { LoginSchema } from '@/schemas';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+
 import { CardWrapper } from '@/components/auth/card-wrapper';
 
 export const LoginForm = () => {
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
   return (
     <CardWrapper
       headerLabel="Welcome back"
