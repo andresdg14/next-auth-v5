df2d21a70342cc4bcdfdb2083f26fa3b594aa83e
Andres <andresdomlez@gmail.com>
2024-08-23 00:26:18 +0100

    Middleware and login

---
+++ routes.ts
@@ -0,0 +1,34 @@
+/**
+ * An array of routes that are accessible to the public
+ * These routes do not require authentication
+ * @type {string[]}
+ */
+export const publicRoutes = ['/'];
+
+/**
+ * An array of routes that are used fop authentication
+ * These routes will redirect logged in users to /settings
+ * @type {string[]}
+ */
+export const authRoutes = ['/auth/login', '/auth/register'];
+
+/**
+ * The prefix for the api authentication routes
+ * Routes that start with this prefix are used for API authentication purposes
+ * @type {string}
+ */
+export const apiAuthPrefix = '/api/auth';
+
+/**
+ * An array of routes that are protected
+ * These routes will redirect to the login page if the user is not authenticated
+ * @type {string[]}
+ */
+export const protectedRoutes = ['/settings'];
+
+/**
+ * The default route to redirect to after a successful login and logout
+ * @type {string}
+ */
+export const DEFAULT_LOGIN_REDIRECT = '/settings';
+export const DEFAULT_LOGOUT_REDIRECT = '/';
