4e36a0ca9656209d104750bfa47e796da8232e89
Andres <andresdomlez@gmail.com>
2024-09-26 00:47:01 +0100

    Callbacks

--- auth.ts
+++ auth.ts
@@ -1,9 +1,44 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 import authConfig from '@/auth.config';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from '@/lib/db';
 
+import { getUserById } from '@/data/user';
+import { UserRole } from '@prisma/client';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      role: string;
+    } & DefaultSession['user'];
+  }
+}
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    // With this we can add the user id to the session, so every placewhere we use session.user.id we can get the user id
+    async session({ token, session }) {
+      //console.log({ sessionToken: token });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      // With this we're making the role available to the session
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+
+      //console.log(token);
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
